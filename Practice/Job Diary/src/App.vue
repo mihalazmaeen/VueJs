ec2491ca76a5385332d5f6c6a1f87d1ef0872016
mihalazmaeen <azmaeenmihal1@gmail.com>
2022-10-26 12:46:22 +0600

    new file:   Practice/Job Diary/.browserslistrc 	new file:   Practice/Job Diary/.eslintrc.js 	new file:   Practice/Job Diary/.gitignore 	new file:   Practice/Job Diary/.prettierrc 	new file:   Practice/Job Diary/babel.config.js 	new file:   Practice/Job Diary/package-lock.json 	new file:   Practice/Job Diary/package.json 	new file:   Practice/Job Diary/public/favicon.ico 	new file:   Practice/Job Diary/public/index.html 	new file:   Practice/Job Diary/src/App.vue 	new file:   Practice/Job Diary/src/components/job-resources/JobResource.vue 	new file:   Practice/Job Diary/src/main.js

---
+++ Practice/Job Diary/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+    <ul>
+        <job-resource v-for="res in storedResources" :key="res.id"
+        :title="res.title" :description="res.description" :link="res.link">
+
+        </job-resource>
+    </ul>
+</template>
+<script>
+import JobResource from './components/job-resources/JobResource.vue';
+
+export default {
+    components: {
+      JobResource  
+    },
+    data() {
+        return {
+            storedResources: [
+                {
+                    id: 'official-guide',
+                    title: 'official-guide',
+                    description: 'The official vue.js documentation',
+                    link:'https://vuejs.org'
+                }, 
+                 {
+                    id: 'google',
+                    title: 'Google',
+                    description: 'Your handy search tool',
+                    link:'https://google.com'
+                }, 
+            ]
+
+        };
+    },
+}
+</script>
